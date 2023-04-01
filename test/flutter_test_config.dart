835a52f7a66871fcc69352ae02664e7bb3d446ff
hawkbee1 <googandads@gmail.com>
2023-04-01 17:23:07 +0200

    better golden testing

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -5,7 +5,10 @@ import 'package:flutter_test/flutter_test.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   final defaultDeviceConfigs = {
+    iPhone8,
     iPhone13,
+    iPadPro,
+    desktop,
     pixel5,
   };
   TestWidgetsFlutterBinding.ensureInitialized();
